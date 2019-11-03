60d8892e330b0d2ec02621515634f3f636532180
Kevin Zhang <kzhaaang@gmail.com>
2019-11-03 06:01:46 -0500

    add forever

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/sh
 
 # java -Xmx1536M -Xms512M -jar server.jar
-java -Xmx3000M -Xms1024M -jar fabric-server-launch.jar
+
+until java -Xmx3000M -Xms1024M -jar fabric-server-launch.jar; do
+	echo "Server crashed with exit code $?, restarting..." >&2
+	sleep 1
+done
 
